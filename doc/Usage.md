3891d2a965b0311799f6c0975c96511a22d82e27
mbednarek360 <mbednarek360@protonmail.com>
2019-11-14 14:56:10 -0500

    Add cli creation interface

---
+++ doc/Usage.md
@@ -0,0 +1,3 @@
+`net create net.yml 3 2 3`
+`net run net.yml inp.yml out.yml`
+`net train net.yml data.yml`
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        
-        Network n = new Network(new int[]{2, 3, 2});
-        n.printNetwork();
-        //double[] out = n.runNetwork(new double[]{0.3, 0.7});
-        //System.out.println("##########################");
-        //for (double val : out) {
-        //    System.out.println(val);
-        //}
-        n.exportNetwork(args[0]);
 
-        Network n1 = new Network(args[0]);
-        n1.printNetwork();
+        // handle command line arguements
+        try {
+
+            // create new random network
+            if (args[0].equals("create")) {
+                int[] layerCounts = new int[args.length - 2];
+                for (int i = 2; i < args.length; i++) {
+                    layerCounts[i - 2] = Integer.parseInt(args[i]);
+                }
+                new Network(layerCounts).exportNetwork(args[1]);
+            }
+
+            // run network with given input
+            if (args[0].equals("run")) {
 
 
+            }
+
+            // train network on dataset
+            if (args[0].equals("train")) { 
+            
+            }
+        }
+
+        catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
